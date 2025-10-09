2fe5559029c27841ea945c0b64510ddd7936fe28
ashesbloom <mayank.pandey232@lpu.in>
2025-10-09 10:52:28 +0530

    added basic variable concepts in js

---
+++ Needed Concepts of JS/variable.js
@@ -0,0 +1,22 @@
+//? Variable Declaration: var, let, const
+
+//? var is function-scoped and can be redeclared and updated
+console.log(name); // undefined
+var name = "First";
+var name = "Last"; // redeclaration is allowed as var is the data-type
+console.log(name); // should print "Last"
+
+//? Block-scoped variables: let and const
+// let and const do not allow redeclaration
+let age = 25;
+console.log(age); // should print 25
+//! let age = 30; 
+// This will throw an error as redeclaration is not allowed with let (within the same block scope)
+age = 30; // reassignment is allowed with let but not redeclaration
+console.log(age); // should print 30
+
+// const is used when the value should not be reassigned
+const pi = 3.14;
+console.log(pi); // should print 3.14
+//! pi = 3.14159; 
+// This will throw an error as reassignment & redeclaration is not allowed with const
